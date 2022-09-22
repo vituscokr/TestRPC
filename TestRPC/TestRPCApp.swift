4bb0c998e2a942380f8351792fef87cfcb033a39
Kyeongtae Nam <vituscokr@gmail.com>
2022-09-22 11:56:15 +0900

    Initial Commit

---
+++ TestRPC/TestRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRPCApp.swift
+//  TestRPC
+//
+//  Created by Gyeongtae Nam on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TestRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
